2c47280fc4eafd4ffceca6c1b16c52b9a1f953ff
Francisco <frankin930@gmail.com>
2026-06-29 19:01:26 -0300

    chore: initialize spring boot project

---
+++ src/main/java/com/proteinevolution/backend/ProteinEvolutionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proteinevolution.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteinEvolutionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteinEvolutionBackendApplication.class, args);
+	}
+
+}
